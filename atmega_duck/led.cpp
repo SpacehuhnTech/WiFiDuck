32bea842c4f4c0881e2bb77d32f1da7743a6974c
Spacehuhn <github@spacehuhn.com>
2019-08-03 15:48:21 +0200

    LED module for atmega duck

---
+++ atmega_duck/led.cpp
@@ -0,0 +1,34 @@
+/*
+   Copyright (c) 2019 Stefan Kremser
+   This software is licensed under the MIT License. See the license file for details.
+   Source: github.com/spacehuhn/WiFiDuck
+ */
+
+#include "led.h"
+
+#include "NeoPixel.h"
+#include "config.h"
+
+namespace led {
+    NeoPixel* led { nullptr };
+
+    void begin() {
+#ifdef NEOPIXEL
+        led = new NeoPixel(NEOPIXEL_NUM, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+        led->begin();
+        led->show();
+#endif // ifdef ENABLE_NEOPIXEL
+    }
+
+    void setColor(int r, int g, int b) {
+  #ifdef NEOPIXEL
+        if (led) {
+            for (size_t i = 0; i<led->numPixels(); i++) {
+                led->setPixelColor(i, r, g, b);
+            }
+
+            led->show();
+        }
+#endif // ifdef ENABLE_NEOPIXEL
+    }
+}
---
+++ atmega_duck/led.h
@@ -0,0 +1,12 @@
+/*
+   Copyright (c) 2019 Stefan Kremser
+   This software is licensed under the MIT License. See the license file for details.
+   Source: github.com/spacehuhn/WiFiDuck
+ */
+
+#pragma once
+
+namespace led {
+    void begin();
+    void setColor(int r, int g, int b);
+}
