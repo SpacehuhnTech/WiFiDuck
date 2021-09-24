a2e1971f0662350873e25b00e12d87d12ee5131b
S1m1B1lly <62600759+S1m1B1lly@users.noreply.github.com>
2021-09-24 13:38:16 +0100

    Adapt duckparser.cpp to work with locale_pt.h

--- atmega_duck/duckparser.cpp
+++ atmega_duck/duckparser.cpp
@@ -173,6 +173,8 @@ namespace duckparser {
                     keyboard::setLocale(&locale_fr);
                 } else if (compare(w->str, w->len, "DK", CASE_SENSETIVE)) {
                     keyboard::setLocale(&locale_dk);
+                } else if (compare(w->str, w->len, "PT", CASE_SENSETIVE)) {
+                    keyboard::setLocale(&locale_pt);
                 }
                 ignore_delay = true;
             }
