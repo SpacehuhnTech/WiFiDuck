c4f29ae095932cf8156871905c523fdafb7a261b
Spacehuhn <github@spacehuhn.de>
2022-01-31 11:19:15 +0100

    Bump version to 1.2.0

--- atmega_duck/config.h
+++ atmega_duck/config.h
@@ -5,7 +5,7 @@
 
 #pragma once
 
-#define VERSION "1.1.0"
+#define VERSION "1.2.0"
 
 /* ===== Serial Bridge ===== */
 // #define BRIDGE_ENABLE
--- esp_duck/config.h
+++ esp_duck/config.h
@@ -5,7 +5,7 @@
 
 #pragma once
 
-#define VERSION "1.1.0"
+#define VERSION "1.2.0"
 
 /*! ===== DEBUG Settings ===== */
 // #define ENABLE_DEBUG
