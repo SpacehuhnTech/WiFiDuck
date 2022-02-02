0914c29fcbb9d83dc802dfe916b421177c9da919
Stefan Kremser <github@spacehuhn.de>
2022-02-02 20:24:01 +0100

    Update locale_cafr.h
    Addressing issue https://github.com/SpacehuhnTech/WiFiDuck/issues/77

--- atmega_duck/locale_cafr.h
+++ atmega_duck/locale_cafr.h
@@ -48,8 +48,8 @@ const uint8_t ascii_cafr[] PROGMEM = {
     // 32, 0x20
     KEY_NONE,           KEY_SPACE,          // ' '
     KEY_MOD_LSHIFT,     KEY_1,              // !
-    KEY_MOD_LSHIFT,     KEY_DOT,            // "
-    KEY_MOD_LSHIFT,     KEY_3,              // #
+    KEY_MOD_LSHIFT,     KEY_2,              // "
+    KEY_NONE,           KEY_GRAVE,          // #
 
     // 36, 0x24
     KEY_MOD_LSHIFT,     KEY_4,              // $
@@ -67,7 +67,7 @@ const uint8_t ascii_cafr[] PROGMEM = {
     KEY_NONE,           KEY_COMMA,          // ,
     KEY_NONE,           KEY_MINUS,          // -
     KEY_NONE,           KEY_DOT,            // .
-    KEY_NONE,           KEY_GRAVE,          // /
+    KEY_MOD_LSHIFT,     KEY_3,              // /
 
     // 48, 0x30
     KEY_NONE,           KEY_0,              // 0
@@ -88,13 +88,13 @@ const uint8_t ascii_cafr[] PROGMEM = {
     KEY_NONE,           KEY_SEMICOLON,      // ;
 
     // 60, 0x3c
-    KEY_MOD_RALT,       KEY_COMMA,          // <
+    KEY_NONE,           KEY_BACKSLASH,      // <
     KEY_NONE,           KEY_EQUAL,          // =
-    KEY_MOD_RALT,       KEY_DOT,            // >
+    KEY_MOD_LSHIFT,     KEY_BACKSLASH,      // >
     KEY_MOD_LSHIFT,     KEY_6,              // ?
 
     // 64, 0x40
-    KEY_MOD_LSHIFT,     KEY_2,              // @
+    KEY_MOD_RALT,       KEY_2,              // @
     KEY_MOD_LSHIFT,     KEY_A,              // A
     KEY_MOD_LSHIFT,     KEY_B,              // B
     KEY_MOD_LSHIFT,     KEY_C,              // C
@@ -133,11 +133,11 @@ const uint8_t ascii_cafr[] PROGMEM = {
     KEY_MOD_LSHIFT,     KEY_X,              // X
     KEY_MOD_LSHIFT,     KEY_Y,              // Y
     KEY_MOD_LSHIFT,     KEY_Z,              // Z
-    KEY_MOD_RALT,       KEY_9,              // [
+    KEY_MOD_RALT,       KEY_LEFTBRACE,      // [
 
     // 92, 0x5c
-    KEY_MOD_LSHIFT,     KEY_GRAVE,          // bslash
-    KEY_MOD_RALT,       KEY_0,              // ]
+    KEY_MOD_RALT,       KEY_GRAVE,          // bslash
+    KEY_MOD_RALT,       KEY_RIGHTBRACE,     // ]
     KEY_NONE,           KEY_SPACE,          // ^
     KEY_MOD_LSHIFT,     KEY_MINUS,          // _
 
@@ -181,78 +181,74 @@ const uint8_t ascii_cafr[] PROGMEM = {
     KEY_NONE,           KEY_X,              // x
     KEY_NONE,           KEY_Y,              // y
     KEY_NONE,           KEY_Z,              // z
-    KEY_MOD_RALT,       KEY_7,              // {
+    KEY_MOD_RALT,       KEY_APOSTROPHE,     // {
 
     // 124, 0x7c
-    KEY_MOD_RALT,       KEY_GRAVE,          // |
-    KEY_MOD_RALT,       KEY_8,              // }
-    KEY_MOD_RALT,       KEY_RIGHTBRACE,     // ~
+    KEY_MOD_LSHIFT,     KEY_GRAVE,          // |
+    KEY_MOD_RALT,       KEY_BACKSLASH,      // }
+    KEY_MOD_RALT,       KEY_SEMICOLON,      // ~
     KEY_NONE,           KEY_DELETE          // DEL
 };
 
 const uint8_t utf8_cafr[] PROGMEM = {
-    0xC2, 0xA4, 0x00, 0x00, KEY_MOD_RALT,           KEY_4,              // ¤
-    0xC2, 0xAB, 0x00, 0x00, KEY_MOD_RALT,           KEY_Z,              // «
-    0xC2, 0xAC, 0x00, 0x00, KEY_MOD_RALT,           KEY_EQUAL,          // ¬
-    0xC2, 0xB0, 0x00, 0x00, KEY_MOD_RALT,           KEY_SEMICOLON,      // °
-    0xC2, 0xBB, 0x00, 0x00, KEY_MOD_RALT,           KEY_X,              // »
-    0xC3, 0x80, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_BACKSLASH,      // À
-    0xC3, 0x87, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_RIGHTBRACE,     // Ç
-    0xC3, 0x88, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_APOSTROPHE,     // È
+    0xC2, 0xA2, 0x00, 0x00, KEY_MOD_RALT,           KEY_4,              // ¢
+    0xC2, 0xA3, 0x00, 0x00, KEY_MOD_RALT,           KEY_3,              // £
+    0xC2, 0xA4, 0x00, 0x00, KEY_MOD_RALT,           KEY_5,              // ¤
+    0xC2, 0xA6, 0x00, 0x00, KEY_MOD_RALT,           KEY_7,              // ¦
+    0xC2, 0xA7, 0x00, 0x00, KEY_MOD_RALT,           KEY_O,              // §
+    0xC2, 0xA8, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_RIGHTBRACE,     // ¨
+    0xC2, 0xAB, 0x00, 0x00, KEY_NONE,               KEY_102ND,          // «
+    0xC2, 0xAC, 0x00, 0x00, KEY_MOD_RALT,           KEY_6,              // ¬
+    0xC2, 0xAD, 0x00, 0x00, KEY_MOD_RALT,           KEY_DOT,            // ­
+    0xC2, 0xAF, 0x00, 0x00, KEY_MOD_RALT,           KEY_COMMA,          // ¯
+    0xC2, 0xB0, 0x00, 0x00, KEY_MOD_RALT,           KEY_102ND,          // °
+    0xC2, 0xB1, 0x00, 0x00, KEY_MOD_RALT,           KEY_1,              // ±
+    0xC2, 0xB2, 0x00, 0x00, KEY_MOD_RALT,           KEY_8,              // ²
+    0xC2, 0xB3, 0x00, 0x00, KEY_MOD_RALT,           KEY_9,              // ³
+    0xC2, 0xB6, 0x00, 0x00, KEY_MOD_RALT,           KEY_P,              // ¶
+    0xC2, 0xB8, 0x00, 0x00, KEY_NONE,               KEY_RIGHTBRACE,     // ¸
+    0xC2, 0xBB, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_102ND,          // »
+    0xC2, 0xBC, 0x00, 0x00, KEY_MOD_RALT,           KEY_0,              // ¼
+    0xC2, 0xBD, 0x00, 0x00, KEY_MOD_RALT,           KEY_MINUS,          // ½
+    0xC2, 0xBE, 0x00, 0x00, KEY_MOD_RALT,           KEY_EQUAL,          // ¾
     0xC3, 0x89, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_SLASH,          // É
-    0xC3, 0x99, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_102ND,          // Ù
-    0xC3, 0xA0, 0x00, 0x00, KEY_NONE,               KEY_BACKSLASH,      // à
-    0xC3, 0xA7, 0x00, 0x00, KEY_NONE,               KEY_RIGHTBRACE,     // ç
-    0xC3, 0xA8, 0x00, 0x00, KEY_NONE,               KEY_APOSTROPHE,     // è
     0xC3, 0xA9, 0x00, 0x00, KEY_NONE,               KEY_SLASH,          // é
-    0xC3, 0xB9, 0x00, 0x00, KEY_NONE,               KEY_102ND,          // ù
-    0xE2, 0x82, 0xAC, 0x00, KEY_MOD_RALT,           KEY_E,              // €
 };
 
 const uint8_t combinations_cafr[] PROGMEM = {
     0x5E, 0x00, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_NONE,           KEY_SPACE,          // ^
-    0x60, 0x00, 0x00, 0x00, KEY_MOD_RALT,           KEY_LEFTBRACE,      KEY_NONE,           KEY_SPACE,          // `
-    0xC2, 0xA8, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_LEFTBRACE,      KEY_NONE,           KEY_SPACE,          // ¨
+    0x60, 0x00, 0x00, 0x00, KEY_NONE,               KEY_APOSTROPHE,     KEY_NONE,           KEY_SPACE,          // `
+    0xC2, 0xB4, 0x00, 0x00, KEY_MOD_RALT,           KEY_SLASH,          KEY_NONE,           KEY_SPACE,          // ´
+    0xC3, 0x80, 0x00, 0x00, KEY_NONE,               KEY_APOSTROPHE,     KEY_MOD_LSHIFT,     KEY_A,              // À
+    0xC3, 0x81, 0x00, 0x00, KEY_MOD_RALT,           KEY_SLASH,          KEY_MOD_LSHIFT,     KEY_A,              // Á
     0xC3, 0x82, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_A,              // Â
-    0xC3, 0x84, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_A,              // Ä
+    0xC3, 0x88, 0x00, 0x00, KEY_NONE,               KEY_APOSTROPHE,     KEY_MOD_LSHIFT,     KEY_E,              // È
     0xC3, 0x8A, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_E,              // Ê
-    0xC3, 0x8B, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_E,              // Ë
-    0xC3, 0x8C, 0x00, 0x00, KEY_MOD_RALT,           KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_I,              // Ì
+    0xC3, 0x8C, 0x00, 0x00, KEY_NONE,               KEY_APOSTROPHE,     KEY_MOD_LSHIFT,     KEY_I,              // Ì
+    0xC3, 0x8D, 0x00, 0x00, KEY_MOD_RALT,           KEY_SLASH,          KEY_MOD_LSHIFT,     KEY_I,              // Í
     0xC3, 0x8E, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_I,              // Î
-    0xC3, 0x8F, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_I,              // Ï
-    0xC3, 0x92, 0x00, 0x00, KEY_MOD_RALT,           KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_O,              // Ò
+    0xC3, 0x92, 0x00, 0x00, KEY_NONE,               KEY_APOSTROPHE,     KEY_MOD_LSHIFT,     KEY_O,              // Ò
+    0xC3, 0x93, 0x00, 0x00, KEY_MOD_RALT,           KEY_SLASH,          KEY_MOD_LSHIFT,     KEY_O,              // Ó
     0xC3, 0x94, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_O,              // Ô
-    0xC3, 0x96, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_O,              // Ö
+    0xC3, 0x99, 0x00, 0x00, KEY_NONE,               KEY_APOSTROPHE,     KEY_MOD_LSHIFT,     KEY_U,              // Ù
+    0xC3, 0x9A, 0x00, 0x00, KEY_MOD_RALT,           KEY_SLASH,          KEY_MOD_LSHIFT,     KEY_U,              // Ú
     0xC3, 0x9B, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_U,              // Û
-    0xC3, 0x9C, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_U,              // Ü
+    0xC3, 0x9D, 0x00, 0x00, KEY_MOD_RALT,           KEY_SLASH,          KEY_MOD_LSHIFT,     KEY_Y,              // Ý
+    0xC3, 0xA0, 0x00, 0x00, KEY_NONE,               KEY_APOSTROPHE,     KEY_NONE,           KEY_A,              // à
+    0xC3, 0xA1, 0x00, 0x00, KEY_MOD_RALT,           KEY_SLASH,          KEY_NONE,           KEY_A,              // á
     0xC3, 0xA2, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_NONE,           KEY_A,              // â
-    0xC3, 0xA4, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_LEFTBRACE,      KEY_NONE,           KEY_A,              // ä
+    0xC3, 0xA8, 0x00, 0x00, KEY_NONE,               KEY_APOSTROPHE,     KEY_NONE,           KEY_E,              // è
     0xC3, 0xAA, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_NONE,           KEY_E,              // ê
-    0xC3, 0xAB, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_LEFTBRACE,      KEY_NONE,           KEY_E,              // ë
-    0xC3, 0xAC, 0x00, 0x00, KEY_MOD_RALT,           KEY_LEFTBRACE,      KEY_NONE,           KEY_I,              // ì
+    0xC3, 0xAC, 0x00, 0x00, KEY_NONE,               KEY_APOSTROPHE,     KEY_NONE,           KEY_I,              // ì
+    0xC3, 0xAD, 0x00, 0x00, KEY_MOD_RALT,           KEY_SLASH,          KEY_NONE,           KEY_I,              // í
     0xC3, 0xAE, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_NONE,           KEY_I,              // î
-    0xC3, 0xAF, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_LEFTBRACE,      KEY_NONE,           KEY_I,              // ï
-    0xC3, 0xB2, 0x00, 0x00, KEY_MOD_RALT,           KEY_LEFTBRACE,      KEY_NONE,           KEY_O,              // ò
+    0xC3, 0xB2, 0x00, 0x00, KEY_NONE,               KEY_APOSTROPHE,     KEY_NONE,           KEY_O,              // ò
+    0xC3, 0xB3, 0x00, 0x00, KEY_MOD_RALT,           KEY_SLASH,          KEY_NONE,           KEY_O,              // ó
     0xC3, 0xB4, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_NONE,           KEY_O,              // ô
-    0xC3, 0xB6, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_LEFTBRACE,      KEY_NONE,           KEY_O,              // ö
+    0xC3, 0xB9, 0x00, 0x00, KEY_NONE,               KEY_APOSTROPHE,     KEY_NONE,           KEY_U,              // ù
+    0xC3, 0xBA, 0x00, 0x00, KEY_MOD_RALT,           KEY_SLASH,          KEY_NONE,           KEY_U,              // ú
     0xC3, 0xBB, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_NONE,           KEY_U,              // û
-    0xC3, 0xBC, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_LEFTBRACE,      KEY_NONE,           KEY_U,              // ü
-    0xC3, 0xBF, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_LEFTBRACE,      KEY_NONE,           KEY_Y,              // ÿ
-    0xC4, 0x88, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_C,              // Ĉ
-    0xC4, 0x89, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_NONE,           KEY_C,              // ĉ
-    0xC4, 0x9C, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_G,              // Ĝ
-    0xC4, 0x9D, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_NONE,           KEY_G,              // ĝ
-    0xC4, 0xA4, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_H,              // Ĥ
-    0xC4, 0xA5, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_NONE,           KEY_H,              // ĥ
-    0xC4, 0xB4, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_J,              // Ĵ
-    0xC4, 0xB5, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_NONE,           KEY_J,              // ĵ
-    0xC5, 0x9C, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_S,              // Ŝ
-    0xC5, 0x9D, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_NONE,           KEY_S,              // ŝ
-    0xC5, 0xB4, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_W,              // Ŵ
-    0xC5, 0xB5, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_NONE,           KEY_W,              // ŵ
-    0xC5, 0xB6, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_Y,              // Ŷ
-    0xC5, 0xB7, 0x00, 0x00, KEY_NONE,               KEY_LEFTBRACE,      KEY_NONE,           KEY_Y,              // ŷ
-    0xC5, 0xB8, 0x00, 0x00, KEY_MOD_LSHIFT,         KEY_LEFTBRACE,      KEY_MOD_LSHIFT,     KEY_Y,              // Ÿ
+    0xC3, 0xBD, 0x00, 0x00, KEY_MOD_RALT,           KEY_SLASH,          KEY_NONE,           KEY_Y,              // ý
 };
 
 static hid_locale_t locale_cafr {
